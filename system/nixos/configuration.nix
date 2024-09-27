99a12be4983d5858275f51c2036b0529842238b6
Elias Mueller <elias.mr1@gmail.com>
2024-09-27 19:23:34 +0200

    Enable docker

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -162,6 +162,19 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  virtualisation.docker = {
+    enable = true;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+
+    daemon.settings = {
+      userland-proxy = false;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
