e5cd7e370951771e9f9dcd50ff127f2efa709e04
Elias Mueller <elias.mr1@gmail.com>
2024-09-28 11:40:21 +0200

    Rewrite configuration.nix

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 {
   config,
   pkgs,
@@ -11,157 +7,56 @@
 }:
 
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  programs = {
+    _1password.enable = true;
+    _1password-gui = {
+      enable = true;
+      # Certain features, including CLI integration and system authentication support,
+      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+      polkitPolicyOwners = [ userConfig.username ];
+    };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    firefox.enable = true;
 
-  networking.hostName = userConfig.hostname; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = userConfig.timezone;
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = userConfig.defaultLocale;
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = userConfig.extraLocale;
-    LC_IDENTIFICATION = userConfig.extraLocale;
-    LC_MEASUREMENT = userConfig.extraLocale;
-    LC_MONETARY = userConfig.extraLocale;
-    LC_NAME = userConfig.extraLocale;
-    LC_NUMERIC = userConfig.extraLocale;
-    LC_PAPER = userConfig.extraLocale;
-    LC_TELEPHONE = userConfig.extraLocale;
-    LC_TIME = userConfig.extraLocale;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+    # mtr.enable = true; # Some programs need SUID wrappers, can be configured further or are started in user sessions
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services = {
+    flatpak.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+    # services.openssh.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = userConfig.keyboardLayout;
-    variant = "";
+    # CUPS
+    printing.enable = true;
+
+    # Audio & Video
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    # Wayland
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      xkb.layout = userConfig.keyboardLayout;
+
+      # libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
+    };
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.elias = {
-    isNormalUser = true;
-    description = userConfig.username;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    packages = with pkgs; [
-      #  thunderbird
-    ];
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  environment.gnome.excludePackages = with pkgs; [
-    epiphany # web browser
-    file-roller # archive manager
-    geary # email client
-    seahorse # password manager
-    simple-scan # document scanner
-    totem # video player
-    yelp # help viewer
-
-    gnome-calendar
-    gnome-contacts
-    gnome-logs
-    gnome-maps
-    gnome-maps
-    gnome-music
-    gnome-screenshot
-
-    pkgs.gnome-tour
-    pkgs.gnome-connections
-  ];
-
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
-
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = [ userConfig.username ];
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   virtualisation.docker = {
     enable = true;
@@ -176,8 +71,34 @@
     };
   };
 
-  services.flatpak.enable = true;
-  xdg.portal.config.common.default = "gtk";
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+    systemPackages = with pkgs; [ ];
+
+    gnome.excludePackages = with pkgs; [
+      epiphany # web browser
+      file-roller # archive manager
+      geary # email client
+      seahorse # password manager
+      simple-scan # document scanner
+      totem # video player
+      yelp # help viewer
+
+      gnome-calendar
+      gnome-connections
+      gnome-contacts
+      gnome-logs
+      gnome-maps
+      gnome-maps
+      gnome-music
+      gnome-screenshot
+      gnome-tour
+    ];
+
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -186,5 +107,70 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = userConfig.hostname;
+    networkmanager.enable = true; # Enable networking
+
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # proxy = {
+    #   default = "http://user:password@proxy:port/";
+    #   noProxy = "127.0.0.1,localhost,internal.domain";
+    # };
+
+    # firewall = {
+    #   enable = false;
+    #   allowedTCPPorts = [ ... ];
+    #   allowedUDPPorts = [ ... ];
+    # };
+  };
+
+  i18n = {
+    defaultLocale = userConfig.defaultLocale;
+
+    extraLocaleSettings = {
+      LC_ADDRESS = userConfig.extraLocale;
+      LC_IDENTIFICATION = userConfig.extraLocale;
+      LC_MEASUREMENT = userConfig.extraLocale;
+      LC_MONETARY = userConfig.extraLocale;
+      LC_NAME = userConfig.extraLocale;
+      LC_NUMERIC = userConfig.extraLocale;
+      LC_PAPER = userConfig.extraLocale;
+      LC_TELEPHONE = userConfig.extraLocale;
+      LC_TIME = userConfig.extraLocale;
+    };
+  };
+
+  time.timeZone = userConfig.timezone; # Set your time zone.
+  hardware.pulseaudio.enable = false; # Enable sound with pipewire.
+  security.rtkit.enable = true; # hands out realtime scheduling priority to user processes on demand
+  xdg.portal.config.common.default = "gtk"; # XDG backend for flatpak
+
+  users.users.elias = {
+    isNormalUser = true;
+    description = userConfig.username;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [ ];
+  };
 
 }
