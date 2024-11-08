561196ec2073ea2503faabee395c3d6d980892cb
Elias Mueller <elias.mr1@gmail.com>
2024-11-08 15:52:33 +0100

    Enable zed

--- setups/fedora.user.nix
+++ setups/fedora.user.nix
@@ -84,6 +84,7 @@
       # jetbrains-toolbox.enable = true;
       # mpv.enable = true;
       telegram-desktop.enable = true;
+      zed.enable = true;
 
     };
 
--- setups/t470p.user.nix
+++ setups/t470p.user.nix
@@ -80,7 +80,7 @@
       # jetbrains-toolbox.enable = true;
       # mpv.enable = true;
       telegram-desktop.enable = true;
-
+      zed.enable = true;
     };
 
   };
