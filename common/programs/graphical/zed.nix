3f88f8e4c8bff79d12021b3989ba98ce3c4a0d14
Elias Mueller <elias.mr1@gmail.com>
2024-11-08 13:59:27 +0100

    Add zed editor

---
+++ common/programs/graphical/zed.nix
@@ -0,0 +1,88 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.myOS.graphical.zed;
+in
+{
+
+  options.myOS.graphical.zed = with lib; {
+
+    enable = mkEnableOption "zed";
+
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    home.packages = with pkgs; [
+      zed-editor
+    ];
+
+    programs.zed-editor = {
+      enable = true;
+
+      userSettings = {
+
+        auto_install_extensions = {
+          "dockerfile" = true;
+          "fish" = true;
+          "git-firefly" = true;
+          "ini" = true;
+          "jinja2" = true;
+          "just" = true;
+          "nix" = true;
+          "nu" = true;
+          "qml" = true;
+          "ruff" = true;
+          "toml" = true;
+        };
+
+        autosave = {
+          after_delay = {
+            "milliseconds" = 1000;
+          };
+        };
+
+        base_keymap = "JetBrains";
+        buffer_font_family = "JetBrainsMono Nerd Font";
+        buffer_font_size = 20;
+
+        git = {
+          inline_blame = {
+            enabled = false;
+          };
+        };
+
+        languages = {
+          Markdown = {
+            remove_trailing_whitespace_on_save = false;
+          };
+        };
+
+        projects_online_by_default = false;
+
+        tab_bar = {
+          show_nav_history_buttons = false;
+        };
+
+        tabs = {
+          git_status = true;
+        };
+
+        theme = {
+          mode = "dark";
+          light = "One Light";
+          dark = "One Dark";
+        };
+
+        ui_font_size = 20;
+
+      };
+    };
+
+  };
+
+}
--- setups/p16gen2.user.nix
+++ setups/p16gen2.user.nix
@@ -83,7 +83,7 @@
       # jetbrains-toolbox.enable = true;
       # mpv.enable = true;
       # telegram-desktop.enable = true;
-
+      zed.enable = true;
     };
 
   };
@@ -96,6 +96,7 @@
       nixgl.nixGLIntel
 
       azure-storage-azcopy
+      nixd
     ];
 
     sessionPath = [
