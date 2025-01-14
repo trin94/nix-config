b2bac412d95a437a2227f1f38003c2f9379aebf1
Elias Mueller <elias.mr1@gmail.com>
2025-01-14 15:56:13 +0100

    p16: re-enable zed

--- setups/p16gen2.user.nix
+++ setups/p16gen2.user.nix
@@ -108,7 +108,7 @@
       # jetbrains-toolbox.enable = true;
       # mpv.enable = true;
       # telegram-desktop.enable = true;
-      # zed.enable = true;
+      zed.enable = true;
     };
 
   };
