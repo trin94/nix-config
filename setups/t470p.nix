dc2479affb6226676c06ae7188cbbde0b92ae1f4
Elias Mueller <elias.mr1@gmail.com>
2025-04-05 22:24:22 +0200

    feat(t470p): add ~/.local/bin to path

--- setups/t470p.nix
+++ setups/t470p.nix
@@ -98,6 +98,10 @@
     packages = with pkgs; [
     ];
 
+    sessionPath = [
+      "$HOME/.local/bin"
+    ];
+
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new Home Manager release introduces backwards
