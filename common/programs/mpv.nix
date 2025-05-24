bcdf794023c12dd8e211751302cfd1609df67d22
Elias Mueller <elias.mr1@gmail.com>
2025-05-24 11:32:15 +0200

    refactor(mpv): copy file instead of symlink

--- common/programs/mpv.nix
+++ common/programs/mpv.nix
@@ -6,6 +6,75 @@
 }:
 let
   cfg = config.myOS.programs.mpv;
+  mpvConfPath = "${config.home.homeDirectory}/.var/app/io.mpv.Mpv/config/mpv/mpv.conf";
+  mpvConf = builtins.toFile "mpv.conf" ''
+    # General
+    keep-open="yes"
+    autofit="65%"
+    cursor-autohide="500"
+
+    # Audio
+    volume="100"
+    volume-max="100"
+
+    # Subs
+    demuxer-mkv-subtitle-preroll="yes"
+    sub-ass-use-video-data="none"
+    sub-fix-timing="yes"
+    sub-font="Open Sans Semibold"
+    # sub-bold="yes"
+    sub-font-size="54"
+    sub-border-size="2.5"
+    sub-margin-y="28"
+    sub-color="#ffffffff"
+
+    # Audio & Subs Prop
+    alang="ja,jp,jpn,en,eng,de,deu,ger"
+    slang="en,eng,de,deu,ger"
+
+    #Screenshot
+    screenshot-format="png"
+    screenshot-high-bit-depth="no"
+    screenshot-png-compression="1"
+    screenshot-jpeg-quality="95"
+    screenshot-template="%f-%wH.%wM.%wS.%wT-#%#00n"
+
+    # Deband
+    deband="no"
+    deband-iterations="4"
+    deband-threshold="50"
+    deband-range="16"
+    deband-grain="0"
+
+    # Resizer
+    scale="ewa_lanczossharp"
+    dscale="catmull_rom"
+    cscale="sinc"
+    cscale-window="blackman"
+    cscale-radius="3"
+
+    [EraiRaws]
+    profile-cond=string.match(get("filename"), "%[Erai%-raws%]")
+    deband="yes"
+
+    [HorribleSubs]
+    profile-cond=string.match(get("filename"), "%[HorribleSubs%]")
+    deband="yes"
+
+    [SubsPlease]
+    profile-cond=string.match(get("filename"), "%[SubsPlease%]")
+    deband="yes"
+
+    [Interpolation]
+    profile-cond=p["container_fps"] <= 24
+    blend-subtitles="yes"
+    video-sync="display-resample"
+    interpolation="yes"
+    tscale="gaussian"
+    tscale-window="sphinx"
+    tscale-radius="1.05"
+    tscale-clamp="0.0"
+  '';
 in
 {
 
@@ -31,76 +100,11 @@ in
 
     programs.mpv.enable = cfg.enable;
 
-    home.file = lib.mkIf cfg.configure {
-      ".var/app/io.mpv.Mpv/config/mpv/mpv.conf".text = ''
-        # General
-        keep-open="yes"
-        autofit="65%"
-        cursor-autohide="500"
-
-        # Audio
-        volume="100"
-        volume-max="100"
-
-        # Subs
-        demuxer-mkv-subtitle-preroll="yes"
-        sub-ass-use-video-data="none"
-        sub-fix-timing="yes"
-        sub-font="Open Sans Semibold"
-        # sub-bold="yes"
-        sub-font-size="54"
-        sub-border-size="2.5"
-        sub-margin-y="28"
-        sub-color="#ffffffff"
-
-        # Audio & Subs Prop
-        alang="ja,jp,jpn,en,eng,de,deu,ger"
-        slang="en,eng,de,deu,ger"
-
-        #Screenshot
-        screenshot-format="png"
-        screenshot-high-bit-depth="no"
-        screenshot-png-compression="1"
-        screenshot-jpeg-quality="95"
-        screenshot-template="%f-%wH.%wM.%wS.%wT-#%#00n"
-
-        # Deband
-        deband="no"
-        deband-iterations="4"
-        deband-threshold="50"
-        deband-range="16"
-        deband-grain="0"
-
-        # Resizer
-        scale="ewa_lanczossharp"
-        dscale="catmull_rom"
-        cscale="sinc"
-        cscale-window="blackman"
-        cscale-radius="3"
-
-        [EraiRaws]
-        profile-cond=string.match(get("filename"), "%[Erai%-raws%]")
-        deband="yes"
-
-        [HorribleSubs]
-        profile-cond=string.match(get("filename"), "%[HorribleSubs%]")
-        deband="yes"
-
-        [SubsPlease]
-        profile-cond=string.match(get("filename"), "%[SubsPlease%]")
-        deband="yes"
-
-        [Interpolation]
-        profile-cond=p["container_fps"] <= 24
-        blend-subtitles="yes"
-        video-sync="display-resample"
-        interpolation="yes"
-        tscale="gaussian"
-        tscale-window="sphinx"
-        tscale-radius="1.05"
-        tscale-clamp="0.0"
-      '';
-    };
+    home.activation.copyMpvConf = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p $(dirname "${mpvConfPath}")
+      cp -f ${mpvConf} "${mpvConfPath}"
+      chmod 444 "${mpvConfPath}"
+    '';
 
   };
 
