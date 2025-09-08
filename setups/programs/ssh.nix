0dd9a55e949930f985bd9436793bd750fc7329fa
Elias Mueller <elias.mr1@gmail.com>
2025-09-08 20:27:51 +0200

    chore: update ssh config
    Move away from deprecated default and extra configs

--- setups/programs/ssh.nix
+++ setups/programs/ssh.nix
@@ -19,23 +19,36 @@ in
 
     programs.ssh = {
       enable = true;
-      extraConfig = ''
-        Host *
-            IdentityAgent ${onePassPath}
+      enableDefaultConfig = false;
 
-        Host home-lab
-            IgnoreUnknown UseKeychain
-            HostName home-lab
-            AddKeysToAgent yes
-            User elias
-            Port 22
-      '';
+      matchBlocks = {
+
+        "home-lab" = lib.hm.dag.entryAfter [ "*" ] {
+          hostname = "home-lab";
+          user = "elias";
+          port = 22;
+          # ignoreUnknown = "UseKeychain";
+          addKeysToAgent = "yes";
+        };
+
+        "*" = {
+          identityAgent = onePassPath;
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+
+      };
+
     };
 
-    home.file.".ssh/config" = {
-      target = ".ssh/config_source";
-      onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
-    };
   };
 
 }
