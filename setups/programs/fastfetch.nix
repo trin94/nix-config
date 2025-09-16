6e0ceb3402a6d868ad45f805da68c487d9d06d7b
Elias Mueller <elias.mr1@gmail.com>
2025-09-16 10:44:53 +0200

    fix(fastfetch): remove obsolete config

--- setups/programs/fastfetch.nix
+++ setups/programs/fastfetch.nix
@@ -27,10 +27,6 @@ in
       settings = {
         "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
-        general = {
-          multithreading = true;
-        };
-
         display = {
           separator = "➜   ";
         };
