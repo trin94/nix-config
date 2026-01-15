40b9579497611f47c1c89b5fbb56e5d909c15281
Elias Mueller <elias.mr1@gmail.com>
2026-01-15 14:32:54 +0100

    chore(mac): configure

--- setups/mac.nix
+++ setups/mac.nix
@@ -7,7 +7,7 @@
 let
   configLocation = "${homeDirectory}/.dotfiles";
   homeDirectory = "/Users/${username}";
-  username = "elias";
+  username = builtins.getEnv "USER";
 in
 {
   imports = dirImport {
@@ -17,8 +17,72 @@ in
   };
 
   myOS = {
+
     programs = {
+
+      alacritty = {
+        enable = true;
+        shell = "${pkgs.fish}/bin/fish";
+        fontSize = 16;
+      };
+
       bat.enable = true;
+      devenv.enable = false;
+      dtrx.enable = true;
+      eza.enable = true;
+      fastfetch.enable = true;
+      fd.enable = true;
+
+      git = {
+        enable = false;
+        configure = true;
+        name = "Elias Mueller";
+        email = builtins.getEnv "EMAIL";
+      };
+
+      fish = {
+        enable = true;
+        configLocation = configLocation;
+        shellInit = ''
+          fish_add_path ~/.nix-profile/bin
+          fish_add_path /nix/var/nix/profiles/default/bin
+          fish_add_path ~/Library/Application Support/JetBrains/Toolbox/scripts
+        '';
+      };
+
+      helix.enable = true;
+      jq.enable = true;
+      just.enable = true;
+
+      kubernetes.enable = true;
+      nh.enable = true;
+      nixfmt.enable = true;
+      nushell.enable = true;
+
+      procs.enable = true;
+      ripgrep.enable = true;
+      sd.enable = true;
+      tokei.enable = true;
+
+      yazi.enable = true;
+      yq.enable = true;
+
+      zed = {
+        enable = false;
+        configure = true;
+      };
+    };
+  };
+
+  stylix = {
+    enable = true;
+    autoEnable = false;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    targets = {
+      fish.enable = true;
+      k9s.enable = true;
+      starship.enable = true;
     };
   };
 
